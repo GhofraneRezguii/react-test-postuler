f6bf1a404c7c1684b2f6a9736f7dcd784d8e40c7
GhofraneRezguii <ghofranerezgui1911@gmail.com>
2025-07-09 17:43:30 +0100

    Nouveaux changements

--- src/Components/JobCard.jsx
+++ src/Components/JobCard.jsx
@@ -1,21 +1,37 @@
+// JobCard.jsx
+// JobCard.jsx
 import React from 'react';
-import './JobCard.css';
 import './JobCard.css';
 
 function JobCard({ title, company, location, link }) {
   return (
     <div className="job-card">
-      <p className="heading">{title}</p>
-      <p>{company}</p>
-      <p>{location}</p>
+      {/* Animation de bordure */}
+      <div className="job-card-bg"></div>
+      <div className="job-card-blob"></div>
 
-      <div className="arrow-link">
-        <a href={link} className="arrow-button">
-          ➜ {/* ou <i className="bi bi-arrow-right"></i> pour Bootstrap Icons */}
-        </a>
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <p className="title">{title}</p>
+          <p>Hover Me</p>
+        </div>
+        <div className="flip-card-back">
+          <p className="title">{company}</p>
+          <p>{location}</p>
+          <div className="arrow-link">
+            <a href={link} className="arrow-button">➜</a>
+          </div>
+
+        </div>
       </div>
+      <div className="arrow-link">
+        <a href={link} className="arrow-button">➜</a>
+      </div>
+    </div>
     </div>
   );
 }
 
 export default JobCard;
+
