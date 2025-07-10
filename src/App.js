7dfd4829a5b1db3901a14f053a1599666334f02f
GhofraneRezguii <ghofranerezgui1911@gmail.com>
2025-07-10 16:38:28 +0100

    jobCard style done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import "./Footer.css";
 import ParticlesBackground from "./Components/ParticlesBackground";
 import Navbar from "./Components/Navbar";
 import Offres from './Components/Offres';
 import SocialMediaIcons from "./Components/SocialMediaIcons";
 import { useTranslation } from "react-i18next";
+// import { useNavigate } from "react-router-dom";
+
 
 function App() {
   const { t, i18n } = useTranslation();
+  // const navigate = useNavigate();
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
@@ -14,50 +18,76 @@ function App() {
 
   return (
     <div>
-      {/* ANIM BACKGROUND */}
       <ParticlesBackground />
-
-      {/* NAVBAR */}
       <Navbar />
 
-      {/* Langue Switcher
-      <div className="text-center mt-4">
-        <button onClick={() => changeLanguage('fr')} className="btn btn-outline-primary mx-2">🇫🇷 Français</button>
-        <button onClick={() => changeLanguage('en')} className="btn btn-outline-secondary mx-2">🇬🇧 English</button>
-      </div> */}
+      <div className="container pt-5" style={{ marginTop: "20px", position: "relative" }}>
+        {/* Bouton à gauche absolue */}
+        <button className="subscribe-btn reversed fixed-left">
+          {/* onClick={() => navigate("/postuler")} */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 5L3 12m0 0l7 7m-7-7h18"
+            />
+          </svg>
+          <p>Retour</p>
+        </button>
 
-      {/* Contenu traduit */}
-      <div className="container text-center pt-5" style={{ marginTop: "20px" }}>
+        {/* Titre centré comme avant */}
         <h1
           style={{
             color: "#072b62",
             lineHeight: "45px",
             margin: 0,
+            marginBottom:"20px",
             fontFamily: "Georgia, serif",
             textShadow: "0 0 10px rgb(110, 164, 235)",
           }}
-          className="pop-up"
+          className="pop-up text-center"
         >
           {t("home.title")}
         </h1>
-        
-        <div className="slide-in-left">
+
+        <div className="slide-in-left text-center">
           <p
             className="slide-in-left gradient-text"
             style={{
               fontFamily: "'Lucida Sans', Geneva, Verdana, sans-serif",
               fontSize: "15px",
-              
             }}
           >
             {t("home.description")}
           </p>
         </div>
+
         <SocialMediaIcons />
         <Offres />
+        {/* <footer style={{ padding: "2rem" }}>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+             <button className="cool-btn">Postuler </button>
+          </div>
+         </footer> */}
+         <footer style={{ padding: "2rem" }}>
+  <div style={{ display: "flex", justifyContent: "flex-end" }}>
+    <button className="styled-footer-btn">
+      Postuler
+      <span></span>
+    </button>
+  </div>
+</footer>
+
       </div>
     </div>
   );
 }
 
 export default App;
+
